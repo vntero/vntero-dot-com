5d23276206ab9bab0580deda3a72858b27ac2f68
vntero <shin.antero@gmail.com>
2021-12-28 10:21:13 +0100

    logo update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,9 @@ function Navbar() {
     return (
         <div>
             <h4>vntero.com</h4>
+            <p>About Me</p>
+            <p>Projects</p>
+            <p>Contact</p>
         </div>
     )
 }
