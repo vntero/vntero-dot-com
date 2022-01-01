d7b410424357674b48c1cfd00c1b2b9e4417a8cc
vntero <shin.antero@gmail.com>
2022-01-01 09:21:13 +0100

    contact form added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,8 +3,23 @@ import React from "react"
 function Contact() {
     return (
         <div className="div-body">
-            <h1>CONTACT</h1>
-            <h3>This page will lodad a form for contact. Fill out the form and press submit to send a request</h3>
+            <div className="contact-form">
+                <form action="" id="message-form">
+                    <label htmlFor="text">Name:</label> <br />
+                    <input type="text" id="name" name="name"/> <br />
+                    <label htmlFor="BLANK SPACE"> </label> <br />
+
+                    <label htmlFor="email">Email:</label> <br />
+                    <input type="email" id="email" name="email"/> <br />
+                    <label htmlFor="BLANK SPACE"> </label> <br />
+
+                    <label htmlFor="message">Message:</label> <br />
+                    <textarea name="message" id="message" cols="45" rows="10" placeholder="What's on your mind?"></textarea> <br />
+                    <label htmlFor="BLANK SPACE"> </label> <br />
+
+                    <button type="submit" form="message-form" value="Submit">Send!</button>
+                </form>
+            </div>
         </div>
     )
 }
