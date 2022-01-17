3b147a4088193271698e85f5889c2613c8a1ee82
vntero <shin.antero@gmail.com>
2022-01-17 14:37:54 +0100

    blockchain content added

--- src/components/Blockchain.jsx
+++ src/components/Blockchain.jsx
@@ -3,6 +3,10 @@ function Blockchain() {
         <div>
             <h1 className="yellow">... work in progress ...</h1>
             <h1>Bitcoin is freedom.</h1>
+             
+             <a href='https://www.preethikasireddy.com/post/the-architecture-of-a-web-3-0-application' target="_blank" rel="noreferrer">
+             <p>The Architecture of a Web 3.0 application</p>
+             </a>
         </div>
     )
 }
