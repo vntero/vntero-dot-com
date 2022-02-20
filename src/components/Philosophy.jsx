a16f68af58ff242a47cebf624f1fa4a289f2a7fb
vntero <shin.antero@gmail.com>
2022-02-20 22:40:04 +0100

    added quotes from flaubert and pessoa

--- src/components/Philosophy.jsx
+++ src/components/Philosophy.jsx
@@ -4,6 +4,12 @@ function Philosophy() {
             <h1 className="yellow">... work in progress ...</h1>
             <h2>“Life can only be understood backwards; but it must be lived forwards.” <br></br>
                 ― Søren Kierkegaard</h2>
+                <label htmlFor="BLANK SPACE"></label><br />
+            <h2>“The artist must be in his work as God is in creation, invisible and all-powerful; one must sense him everywhere but never see him.” <br></br>
+                ― Gustave Flaubert</h2>
+                <label htmlFor="BLANK SPACE"></label><br />
+            <h2>“Jamais existiu alma mais afectuosa ou terna do que a minha, alma mais repleta de bondade, de compaixão, de tudo o que é ternura e amor. Porém, nenhuma alma é tão solitária como a minha - solitária, note-se não devido a circunstâncias externas mas sim a circunstâncias internas.” <br></br>
+                ― Fernando Pessoa</h2>
         </div>
     )
 }
