3f4e04ab9034ae8dc32c9f4bb86e3043ed154b3e
vntero <shin.antero@gmail.com>
2022-02-28 18:48:57 +0100

    logo updates and languages too

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -24,16 +24,16 @@ function About() {
 
             <div className="div-skills">
                 <a href="https://www.javascript.com/" target="_blank" rel="noreferrer"><img alt="JavaScript" src="javascript.png" height={50} width={50}/></a>
-                <a href="https://reactjs.org/" target="_blank" rel="noreferrer"><img alt="React" src="react.png" height={50} width={50}/></a>
-                <a href="https://nodejs.org/" target="_blank" rel="noreferrer"><img alt="Node" src="node.png" height={50} width={50}/></a>
-                <a href="https://expressjs.com/" target="_blank" rel="noreferrer"><img alt="Express" src="express.png" height={50} width={80}/></a>
+                <a href="https://reactjs.org/" target="_blank" rel="noreferrer"><img alt="React" src="react.png" height={45} width={50}/></a>
+                <a href="https://nodejs.org/" target="_blank" rel="noreferrer"><img alt="Node" src="node.png" height={50} width={80}/></a>
+                <a href="https://expressjs.com/" target="_blank" rel="noreferrer"><img alt="Express" src="express.png" height={20} width={80}/></a>
                 <a href="https://nextjs.org/" target="_blank" rel="noreferrer"><img alt="Next" src="nextjs.png" height={45} width={75}/></a>
                 
             </div>
             <div className="div-skills">
                 <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer"><img alt="MongoDB" src="mongodb.png" height={50} width={80}/></a>
-                <a href="https://html.spec.whatwg.org/" target="_blank" rel="noreferrer"><img alt="HTML" src="html.png" height={50} width={50}/></a>
-                <a href="https://www.w3.org/TR/CSS/#css" target="_blank" rel="noreferrer"><img alt="CSS" src="css.png" height={50} width={50}/></a>
+                <a href="https://laravel.com/" target="_blank" rel="noreferrer"><img alt="Laravel" src="laravel.png" height={50} width={50}/></a>
+                <a href="https://www.postgresql.org/" target="_blank" rel="noreferrer"><img alt="PostgreSQL" src="pgsql.png" height={50} width={50}/></a>
                 <a href="https://mui.com/" target="_blank" rel="noreferrer"><img alt="Material-UI" src="mui.png" height={50} width={50}/></a>
                 <a href="https://nextui.org" target="_blank" rel="noreferrer"><img alt="NextUI" src="nextui.png" height={50} width={50}/></a>
             </div>
