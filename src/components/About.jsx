93e2990921a084a1f11844db836e049c567dfaf3
vntero <shin.antero@gmail.com>
2024-06-03 11:36:40 +0200

    changes landing page structure, adds fav projects

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,28 +1,32 @@
 import Avatar from '@mui/material/Avatar'
 import * as React from 'react'
-import { styled } from '@mui/material/styles'
 import Box from '@mui/material/Box'
-import Paper from '@mui/material/Paper'
 import Grid from '@mui/material/Grid'
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 
-const Item = styled(Paper)(({ theme }) => ({
+const StyledPaper = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}))
+  padding: theme.spacing(2),
+  maxWidth: 400,
+  color: theme.palette.text.primary,
+}));
+
+const message = `Truncation should be conditionally applicable on this long line of text
+ as this is a much longer line than what the container can support. `;
 
 function About() {
     return (
       <Box sx={{ flexGrow: 1 }}>
-        <div className="div-body-padding">
-            <Grid container spacing={2}>
-                <Grid item xs={6} md={6}>
+        <div className="div-intro-padding">
+            <Grid container wrap="nowrap" spacing={2}>
+                <Grid xs={6} md={6}>
                     <h1>backend developer looking to build and collaborate on <br />
                     <span className="blue"> beautiful</span>, <span className="red">engaging</span>, and <span className="green">functional</span> projects for the world wide web.</h1>
                 </Grid>
-                <Grid item xs={6} md={6}>
+                <Grid xs={6} md={6}>
                 <div>
                   <div className="div-avatar">
                     <Avatar alt="Hugo Antero" src="headshot.png" sx={{ width: 100, height: 100 }} />
@@ -36,28 +40,55 @@ function About() {
                 </div>
                 </Grid>
             </Grid>
-            
+        </div>
 
-            <label htmlFor="BLANK SPACE"></label><br />
-            <label htmlFor="BLANK SPACE"></label><br />
+        <div className="div-favourite-projects">
+          <h1>favourite projects</h1>
+          <Box sx={{ flexGrow: 1, overflow: 'hidden', px: 3 }}>
+          <Grid container wrap="nowrap" spacing={2} justifyContent="center">
+            <StyledPaper sx={{ my: 1, mx: 'auto', p: 2 }}>
+              <Grid container wrap="nowrap" spacing={2}>
+                <Grid item xs>
+                  <Typography>Project 1 name, description, tech stack</Typography>
+                </Grid>
+              </Grid>
+            </StyledPaper>
+            <StyledPaper sx={{ my: 1, mx: 'auto', p: 2 }}>
+              <Grid container wrap="nowrap" spacing={2}>
+                <Grid item xs>
+                  <Typography>Project 1 name, description, tech stack</Typography>
+                </Grid>
+              </Grid>
+            </StyledPaper>
+            <StyledPaper sx={{ my: 1, mx: 'auto', p: 2 }}>
+              <Grid container wrap="nowrap" spacing={2}>
+                <Grid item xs>
+                  <Typography>Project 1 name, description, tech stack</Typography>
+                </Grid>
+              </Grid>
+            </StyledPaper>
+            </Grid>
+          </Box>
+        </div>
 
-            <h2>Cards up my sleeve:</h2>
+        <div className="div-hard-skills">
+          <h1>cards up my sleeve</h1>
 
-            <div className="div-skills">
-                <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer"><img alt="TypeScript" src="typescript.png" height={45} width={45}/></a>
-                <a href="https://nodejs.org/" target="_blank" rel="noreferrer"><img alt="Node" src="node.png" height={50} width={80}/></a>
-                <a href="https://go.dev" target="_blank" rel="noreferrer"><img alt="Go" src="go.png" height={35} width={85}/></a>
-                <a href="https://reactjs.org/" target="_blank" rel="noreferrer"><img alt="React" src="react.png" height={45} width={50}/></a>
-                <a href="https://jestjs.io/" target="_blank" rel="noreferrer"><img alt="Jest" src="jest.png" height={50} width={50}/></a>
-            </div>
+          <div className="div-skills">
+              <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer"><img alt="TypeScript" src="typescript.png" height={45} width={45}/></a>
+              <a href="https://nodejs.org/" target="_blank" rel="noreferrer"><img alt="Node" src="node.png" height={50} width={80}/></a>
+              <a href="https://go.dev" target="_blank" rel="noreferrer"><img alt="Go" src="go.png" height={35} width={85}/></a>
+              <a href="https://reactjs.org/" target="_blank" rel="noreferrer"><img alt="React" src="react.png" height={45} width={50}/></a>
+              <a href="https://jestjs.io/" target="_blank" rel="noreferrer"><img alt="Jest" src="jest.png" height={50} width={50}/></a>
+          </div>
 
-            <div className="div-skills">
-                <a href="https://graphql.org/" target="_blank" rel="noreferrer"><img alt="AWS" src="gql.png" height={50} width={50}/></a>
-                <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer"><img alt="MongoDB" src="mongo.png" height={70} width={60}/></a>
-                <a href="https://www.postgresql.org/" target="_blank" rel="noreferrer"><img alt="PostgreSQL" src="pgsql.png" height={50} width={50}/></a>
-                <a href="https://www.docker.com/" target="_blank" rel="noreferrer"><img alt="Docker" src="docker.png" height={45} width={60}/></a>
-                <a href="https://aws.amazon.com/" target="_blank" rel="noreferrer"><img alt="AWS" src="aws.png" height={40} width={60}/></a>
-            </div>
+          <div className="div-skills">
+              <a href="https://graphql.org/" target="_blank" rel="noreferrer"><img alt="AWS" src="gql.png" height={50} width={50}/></a>
+              <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer"><img alt="MongoDB" src="mongo.png" height={70} width={60}/></a>
+              <a href="https://www.postgresql.org/" target="_blank" rel="noreferrer"><img alt="PostgreSQL" src="pgsql.png" height={50} width={50}/></a>
+              <a href="https://www.docker.com/" target="_blank" rel="noreferrer"><img alt="Docker" src="docker.png" height={45} width={60}/></a>
+              <a href="https://aws.amazon.com/" target="_blank" rel="noreferrer"><img alt="AWS" src="aws.png" height={40} width={60}/></a>
+          </div>
         </div>
       </Box>
     )
