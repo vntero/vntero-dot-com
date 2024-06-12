4d866d3b2aee59076a8fff876d8ce503841d2167
vntero <shin.antero@gmail.com>
2024-06-12 18:10:12 +0200

    removes extra grid

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,53 +21,46 @@ const card = (
 function About() {
     return (
       <Container>
-        <Grid
-					as="section"
-					columns={{ '@bp1': 2, '@bp2': 3 }}
-					css={{ '@bp1': { minHeight: '80vh' }, alignItems: 'center', gap: '$min' }}
-				>
-      <Stack spacing={4}>
-        <Grid container spacing={2} sx={{ minHeight: { xs: 'auto', md: '80vh' }, alignItems: 'center' }}>
-          {/* Introduction */}
-          <Grid item xs={12} md={8} lg={6} sx={{ padding: { xs: 4, md: 0 } }}>
-            <Stack spacing={1} sx={{ paddingY: { xs: 4, md: 0 } }}>
-              <Box sx={{ paddingX: 2 }}>
-                  <Balancer>
-                    <h1 className='h1-no-margin'>
-                      I'm a backend developer looking to build and collaborate on
-                      <span className="blue"> beautiful</span>, 
-                      <span className="red"> engaging</span>, and 
-                      <span className="green"> functional</span> projects for the world wide web.
-                    </h1>
-                  </Balancer>
-              </Box>
-            </Stack>
-          </Grid>
+          <Stack spacing={4}>
+            <Grid container spacing={2} sx={{ minHeight: { xs: 'auto', md: '80vh' }, alignItems: 'center' }}>
+              {/* Introduction */}
+              <Grid item xs={12} md={8} lg={6} sx={{ padding: { xs: 4, md: 0 } }}>
+                <Stack spacing={1} sx={{ paddingY: { xs: 4, md: 0 } }}>
+                  <Box sx={{ paddingX: 2 }}>
+                      <Balancer>
+                        <h1 className='h1-no-margin'>
+                          I'm a backend developer looking to build and collaborate on
+                          <span className="blue"> beautiful</span>, 
+                          <span className="red"> engaging</span>, and 
+                          <span className="green"> functional</span> projects for the world wide web.
+                        </h1>
+                      </Balancer>
+                  </Box>
+                </Stack>
+              </Grid>
 
-          {/* Name, quotes generator, socials*/}
-          <Grid item xs={12} md={8} lg={6} sx={{ padding: { xs: 4, md: 0 } }}>
-            <Stack spacing={1} sx={{ paddingY: { xs: 4, md: 0 } }}>
-              <Box sx={{ paddingX: 2 }}>
-                <Typography>
-                  <Balancer>
-                    <Avatar alt="Hugo Antero" src="headshot.png" sx={{ width: 80, height: 80 }} />
-                    <h3>Hugo Antero</h3>
-                    <Box sx={{ minWidth: 250 }}>
-                      <Card variant="outlined">{card}</Card>
-                    </Box><br></br>
-                    <a className='margin' href="https://github.com/vntero" target="_blank" rel="noreferrer"><img alt="github" src="github.png" height={25} width={25}/></a>
-                    <a className='margin' href="https://www.linkedin.com/in/vntero/" target="_blank" rel="noreferrer"><img alt="linkedin" src="linkedin.png" height={25} width={25}/></a>
-                    <a className='margin' href="mailto:hi@vntero.com"><img alt="email" src="email.png" height={25} width={25}/></a>
-                  </Balancer>
-                </Typography>
-              </Box>
-            </Stack>
-          </Grid>
-        </Grid>
-      </Stack>
-      </Grid>
-      
-    </Container>
+              {/* Name, quotes generator, socials*/}
+              <Grid item xs={12} md={8} lg={6} sx={{ padding: { xs: 4, md: 0 } }}>
+                <Stack spacing={1} sx={{ paddingY: { xs: 4, md: 0 } }}>
+                  <Box sx={{ paddingX: 2 }}>
+                    <Typography>
+                      <Balancer>
+                        <Avatar alt="Hugo Antero" src="headshot.png" sx={{ width: 80, height: 80 }} />
+                        <h3>Hugo Antero</h3>
+                        <Box sx={{ minWidth: 250 }}>
+                          <Card variant="outlined">{card}</Card>
+                        </Box><br></br>
+                        <a className='margin' href="https://github.com/vntero" target="_blank" rel="noreferrer"><img alt="github" src="github.png" height={25} width={25}/></a>
+                        <a className='margin' href="https://www.linkedin.com/in/vntero/" target="_blank" rel="noreferrer"><img alt="linkedin" src="linkedin.png" height={25} width={25}/></a>
+                        <a className='margin' href="mailto:hi@vntero.com"><img alt="email" src="email.png" height={25} width={25}/></a>
+                      </Balancer>
+                    </Typography>
+                  </Box>
+                </Stack>
+              </Grid>
+            </Grid>
+          </Stack>
+      </Container>
     )
 }
 
