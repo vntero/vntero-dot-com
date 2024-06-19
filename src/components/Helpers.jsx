a270d409e6c21739db0a69ca9209f7932f797a9e
vntero <shin.antero@gmail.com>
2024-06-19 14:16:54 +0200

    puts cs lewis quote in first place

--- src/components/Helpers.jsx
+++ src/components/Helpers.jsx
@@ -2,8 +2,8 @@ import { Paper } from '@mui/material'
 import { styled } from '@mui/material/styles'
 
 export const quotes = [
-  '"We must cultivate our garden." - Voltaire',
   '"We realize it best when we talk with an unliterary friend. He may be full of goodness and good sense but he inhabits a tiny world. In it, we should be suffocated. The man who is contented to be only himself, and therefore less a self, is in prison." - C.S. Lewis',
+  '"We must cultivate our garden." - Voltaire',
   '"My own eyes are not enough for me, I will see through those of others. Reality, even seen through the eyes of many, is not enough. I will see what others have invented." - C.S. Lewis',
   '"Repetition and recollection are the same movement, except in opposite directions, for what is recollected has been, is repeated backward, whereas genuine repetition is recollected forward." - Kierkegaard',
   '"I suffer from life and from other people. I cant look at reality face to face. Even the sun discourages and depresses me. Only at night and all alone, withdrawn, forgotten and lost, with no connection to anything real or useful — only then do I find myself and feel comforted." - Pessoa',
