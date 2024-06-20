08213ca24e331758f23851e4b45ee2d3dc71cfc7
vntero <shin.antero@gmail.com>
2024-06-20 14:55:55 +0200

    installs more mui components

--- src/App.js
+++ src/App.js
@@ -8,7 +8,17 @@ import About from "./components/About"
 import Footer from "./components/Footer"
 import Projects from "./components/Projects"
 
+// dark theme
+import { ThemeProvider, createTheme } from "@mui/material/styles"
+import CssBaseline from "@mui/material/CssBaseline"
+
 function App() {
+  const darkTheme = createTheme({
+    palette: {
+      mode: darkMode ? "dark" : "light",
+    },
+  })
+
   return (
     <div className="App">
       <Routes>
