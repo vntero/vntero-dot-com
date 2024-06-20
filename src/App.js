e9bb9004b3cbf8dae18dd35b8984595e8443c380
vntero <shin.antero@gmail.com>
2024-06-20 16:23:04 +0200

    removes dark mode for now

--- src/App.js
+++ src/App.js
@@ -13,31 +13,31 @@ import { ThemeProvider, createTheme } from "@mui/material/styles"
 import CssBaseline from "@mui/material/CssBaseline"
 
 function App() {
-  const darkTheme = createTheme({
-    palette: {
-      mode: "dark",
-      primary: {
-        main: "#90caf9",
-      },
-      secondary: {
-        main: "#f48fb1",
-      },
-    },
-  })
+  // const darkTheme = createTheme({
+  //   palette: {
+  //     mode: "dark",
+  //     primary: {
+  //       main: "#90caf9",
+  //     },
+  //     secondary: {
+  //       main: "#f48fb1",
+  //     },
+  //   },
+  // })
 
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<About />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-        </Routes>
+    // <ThemeProvider theme={darkTheme}>
+    //   <CssBaseline />
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
 
-        <Footer />
-      </div>
-    </ThemeProvider>
+      <Footer />
+    </div>
+    // </ThemeProvider>
   )
 }
 
