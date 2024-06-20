b82edc1e8729010d42dfe14af047bfd1ec4f9194
vntero <shin.antero@gmail.com>
2024-06-20 21:38:30 +0200

    comments out dark theme

--- src/App.js
+++ src/App.js
@@ -14,17 +14,17 @@ import { createTheme } from "@mui/material/styles"
 // import CssBaseline from "@mui/material/CssBaseline"
 
 function App() {
-  const darkTheme = createTheme({
-    palette: {
-      mode: "dark",
-      primary: {
-        main: "#90caf9",
-      },
-      secondary: {
-        main: "#f48fb1",
-      },
-    },
-  })
+  // const darkTheme = createTheme({
+  //   palette: {
+  //     mode: "dark",
+  //     primary: {
+  //       main: "#90caf9",
+  //     },
+  //     secondary: {
+  //       main: "#f48fb1",
+  //     },
+  //   },
+  // })
 
   return (
     // <ThemeProvider theme={darkTheme}>
